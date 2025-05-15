8fc02e8ec7b3750d715fa12f81a76dd3809e60ed
Giovanni Maya <giovannimhern@uchicago.edu>
2025-05-14 23:12:31 -0500

    feat: gadarian analysis included

--- scripts/anes-analysis.R
+++ scripts/anes-analysis.R
@@ -7,46 +7,16 @@ library(readr)
 setwd("/Users/giomhern/04 Projects/topic-models")
 
 
+documents <- readLdac("data/final_anes.csv")
 meta <- read.csv("data/final_anes_metadata.csv")
 vocab <- read.csv("data/final_anes_vocab.csv")$term
+documents <- documents[meta$pid_summary > 0 & meta$highest.grade.completed > 0 & meta$age > 0]
+meta <- meta[meta$pid_summary > 0 & meta$highest.grade.completed > 0 & meta$age > 0, ]
 
-fix_ldac_file <- function(original_path, output_path) {
-    lines <- readLines(original_path, warn = FALSE)
-    writeLines(c(lines, ""), con = output_path)
-}
-
-read_ldac_custom <- function(path) {
-    lines <- readLines(path)
-    lapply(lines, function(line) {
-        tokens <- strsplit(line, " ")[[1]]
-        tokens <- tokens[-1] # remove token count
-        pairs <- strsplit(tokens, ":")
-        word_ids <- as.integer(sapply(pairs, `[[`, 1)) + 1
-        counts <- as.integer(sapply(pairs, `[[`, 2))
-        rbind(word_ids, counts)
-    })
-}
-
-fix_ldac_file("data/final_anes.csv", "data/final_anes_cleaned.ldac")
-documents <- read_ldac_custom("data/final_anes_cleaned.ldac")
-
-labelTopics(stm_mod, n = 10)
-keep <- with(meta, pid_summary > 0 & `highest.grade.completed` > 0 & age > 0)
-documents <- documents[keep]
-meta <- meta[keep, ]
-
-non_empty_docs <- sapply(documents, function(x) {
-    !is.null(x) && ncol(x) > 0 && sum(x[2, ]) > 0
-})
-
-documents <- documents[non_empty_docs]
-meta <- meta[non_empty_docs, ]
-out <- prepDocuments(documents, vocab, meta)
-documents <- out$documents
-vocab <- out$vocab
-meta <- out$meta
-
-
+data <- prepDocuments(documents = documents, vocab = vocab, meta = meta)
+documents <- data$documents
+vocab <- data$vocab
+meta <- data$meta
 stm_mod <- stm(documents, vocab,
     K = 60,
     prevalence = ~ s(pid_summary) + s(age) + s(`highest.grade.completed`) + s(`highest.grade.completed` * pid_summary),
@@ -79,7 +49,7 @@ plot.estimateEffect(
     covariate = "highest.grade.completed",
     model = stm_mod,
     method = "continuous",
-    topics = 59,
+    topics = 40,
     moderator = "pid_summary",
     moderator.value = 1,
     linecol = "blue",
@@ -95,7 +65,7 @@ plot.estimateEffect(
     covariate = "highest.grade.completed",
     model = stm_mod,
     method = "continuous",
-    topics = 59,
+    topics = 40,
     moderator = "pid_summary",
     moderator.value = 4,
     linecol = "red",
@@ -105,9 +75,5 @@ plot.estimateEffect(
 text(16.2, 0.085, "Democrat", col = "blue", cex = 1.2)
 text(14.1, 0.035, "Republican", col = "red", cex = 1.2)
 dev.off()
-
-
-plot(stm_mod, type = "labels", labeltype = "frex", n = 10)
-plot(stm_mod, type = "perspectives", topics = c(2, 4))
 
 # nolint end
