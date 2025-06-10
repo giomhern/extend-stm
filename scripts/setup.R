2ae4e61ea6c4fd49fd45c4094e3617a490eea271
Giovanni Maya <giovannimhern@uchicago.edu>
2025-06-09 22:40:52 -0700

    docs: final report + presentation proposal

--- scripts/setup.R
+++
@@ -1,9 +0,0 @@
-# nolint start
-
-load("data/gadarian.RData")
-ls()
-
-str(gadarian)
-export_df <- gadarian[, c("MetaID", "treatment", "pid_rep", "open.ended.response")]
-write.csv(export_df, "gadarian_bertopic_input.csv", row.names = FALSE)
-# nolint end
